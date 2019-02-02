cac8120fcf450454d63b59936767ecd2e5917911
George Miao <georgemiao@outlook.com>
2019-02-01 22:17:41 -0500

    2

--- blueprints/home.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-
-profile = Blueprint('profile', __name__)
-
-@app.route('/')
-def home():
-    number = bid(limit=True)
-    content = generate(number)
-    return render_template('content.html', number=number, title="Home", content=content)
