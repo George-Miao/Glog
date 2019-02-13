9c95be858235f7169466d69c20752bae7a8e74b9
George Miao <georgemiao@outlook.com>
2019-02-12 19:36:32 -0500

    ip

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from Main import database
 from Main import db
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port = 80, debug=1)
+    app.run(host="0.0.0.0", port = 80, debug=1)
     db.disconnect()
